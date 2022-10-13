3d9c9718b5775630be8ba1e9f1f10f2ba780efa8
Evan Phair <50122734+evanphair30@users.noreply.github.com>
2022-10-13 19:18:34 -0400

    Update final.py

--- Final Flask/final.py
+++ Final Flask/final.py
@@ -14,7 +14,7 @@ def home():
 
 
 @app.route('/roster', methods=['GET', 'POST'])
-def api_roster():
+def roster():
     if request.method == 'GET':
         return make_response(jsonify(data), 200)
     elif request.method == 'POST':
@@ -22,10 +22,11 @@ def api_roster():
         roster_id = content['id']
         data[roster_id] = content
         data_obj = data.get(roster_id, {})
+        print(data_obj)
         return make_response(jsonify(data_obj), 201)
 
-@app.route('/lineup/<roster_id>', methods=['GET', 'PUT', 'DELETE'])
-def api_each_book(roster_id):
+@app.route('/roster/<roster_id>', methods=['GET', 'PUT', 'DELETE'])
+def each_roster(roster_id):
     if request.method == 'GET':
         data_obj = data.get(roster_id, {})
         if data_obj:
@@ -34,13 +35,14 @@ def api_each_book(roster_id):
             return make_response(jsonify(data_obj), 404)
     elif request.method == 'PUT':
         content = request.json
-        data['id'] = content
+        data[roster_id] = content
         data_obj = data.get(roster_id, {})
         return make_response(jsonify(data_obj), 200)
 #    elif request.method == 'DELETE':
 #        if roster_id in data:
 #            del data(roster_id)
 #        return make_response(jsonify({}),204)
-  
+
+
 if __name__ == "__main__":
     app.run(debug=True)

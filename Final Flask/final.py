197faf5577baede01fc5a2f233f9aa5527d4a9cb
Evan Phair <50122734+evanphair30@users.noreply.github.com>
2022-10-12 21:09:07 -0400

    Update final.py

--- Final Flask/final.py
+++ Final Flask/final.py
@@ -1,32 +1,53 @@
-from flask import Flask, render_template, make_response, jsonify, request, redirect
+from flask import Flask, render_template, make_response, jsonify, request, redirect, json
 import sqlite3
-  
+
 app = Flask(__name__)
+
+with open('football.json') as json_file:
+    data = json.load(json_file)
+
+print (data)
 
 @app.route("/")
 def home():
     return render_template('home.html',title="Home Page")
 
 # Pass the required route to the decorator.
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+#@app.route('/result',methods = ['POST', 'GET'])
+#def result():
+#   if request.method == 'POST':
+#      result = request.form
+#      return render_template("result.html",result = result)
+
+@app.route('/results',methods = ['POST', 'GET'])
+def data():
+    data = request.get_json
+    test = request.form
+    print(test)
+    return jsonify(request.form)
+
+#@app.route('/result',methods = ['POST', 'GET'])
+#def data():
+#    data = request.get_json
+#    print(data)
+#    return jsonify(request.form)
 
 @app.route("/edit",methods = ['PUT', 'GET'])
 def edit():
     if request.method == 'PUT':
         return "Homepage of GeeksForGeeks"
     elif request.method == 'GET':
-        return "Hello"
+        data = request.get_json
+        test = request.form
+        print(test)
+        return jsonify(request.form)
 
 @app.route("/remove",methods = ['DELETE'])
 def remove():
-    if request.method == 'DELETE':
-        return "Homepage of GeeksForGeeks"
+    return render_template('remove.html',title="Remove")
+        
 
-@app.route("/home/<home_id>", methods=['GET', 'PUT', 'DELETE'])
+@app.route("/<home_id>", methods=['GET', 'PUT', 'DELETE'])
 def individual():
     if request.method == 'GET':
         return "Homepage of GeeksForGeeks"
